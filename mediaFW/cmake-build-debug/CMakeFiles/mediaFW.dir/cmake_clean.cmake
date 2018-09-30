70c3796409bdf6c19fdc8d751bc81a24bb15ec8b
Marthina Joensson <marthina.j@gmail.com>
2018-09-30 14:21:07 +0200

    add project

---
+++ mediaFW/cmake-build-debug/CMakeFiles/mediaFW.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mediaFW.dir/main.cpp.o"
+  "mediaFW.pdb"
+  "mediaFW"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mediaFW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mediaFW/cmake-build-debug/CMakeFiles/mediaFW.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mediaFW.
+# This may be replaced when dependencies are built.
