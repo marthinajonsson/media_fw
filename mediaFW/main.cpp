3ef7038a884eb7181e1cfa22bf6493b9548ef902
Marthina Joensson <marthina.j@gmail.com>
2018-09-30 14:33:52 +0200

    first architecture

--- mediaFW/main.cpp
+++ mediaFW/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+class Media{
+public:
+    Media(){
+        std::cout<<"media" <<std::endl;
+        connect();
+    }
+
+private:
+    void connect(){
+        std::cout<<"Connecting.."<<std::endl;
+        //TODO: Setup connection to Network
+    }
+
+};
+
+class Cli {
+public:
+    Cli() {
+        std::cout<<"cli" <<std::endl;
+        cliDeamon();
+    };
+    ~Cli(){}
+private:
+    void cliDeamon(){
+        std::cout<<"cli daemon started" <<std::endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Worlds!" << std::endl;
+
+    Media mm;
+
+    Cli cli;
     return 0;
 }
