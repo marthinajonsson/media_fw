6cff55934232c856839da4faeaa735f905cd61c9
Marthina <marthina.j@gmail.com>
2018-11-18 17:48:42 +0100

    deleted binaries

--- mediaFW/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mjonsson/repo/media_fw/mediaFW
-# Build directory: /home/mjonsson/repo/media_fw/mediaFW/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("contrib/googletest")
--- mediaFW/cmake-build-debug/contrib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mjonsson/repo/media_fw/mediaFW/contrib/googletest
-# Build directory: /home/mjonsson/repo/media_fw/mediaFW/cmake-build-debug/contrib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
